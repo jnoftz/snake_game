396a0b4bab360e4135ab4717651dc8bf2f68b128
Jerry Noftz <noftz@underworldsystems.com>
2021-11-02 15:11:02 -0400

    project break down

--- snake.py
+++ snake.py
@@ -4,3 +4,55 @@ import pygames
 import tkinter as tk
 from tkinter import messagebox
 
+class cude(object):
+    rows = 0
+    w = 0
+    def __init__(self, start, dirnx=1, dirny=0, color=(255,0,0)):
+        pass
+
+    def move(self, dirnx, dirny):
+        pass
+
+    def draw(self, surface, eyes=false):
+        pass
+
+
+class snake(object):
+    def __init__(self, color, pos):
+        pass
+
+    def move(self):
+        pass
+
+    def reset(self, pos):
+        pass
+
+    def add_cube(self):
+        pass
+
+    def draw(self, surface):
+        pass
+
+def draw_grid(w, rows, surface):
+    pass
+
+def redraw_window(surface):
+    pass
+
+def random_snack(rows, items):
+    pass
+
+def message_box(subject, content):
+    pass
+
+def main():
+    pass
+
+rows =
+w = 
+h = 
+
+cude.rows = rows
+cube.w = w
+
+main()
